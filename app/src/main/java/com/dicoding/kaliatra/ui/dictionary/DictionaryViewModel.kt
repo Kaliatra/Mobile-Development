7860132dc752bc566ef09a0aeb54a5d35de9caee
pebrieriawan <pebrieriawan13@gmail.com>
2024-11-30 22:16:57 +0800

    Update dictionary page layout

--- app/src/main/java/com/dicoding/kaliatra/ui/dictionary/DictionaryViewModel.kt
+++ app/src/main/java/com/dicoding/kaliatra/ui/dictionary/DictionaryViewModel.kt
@@ -1,7 +1,5 @@
 package com.dicoding.kaliatra.ui.dictionary
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DictionaryViewModel : ViewModel() {
