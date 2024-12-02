8aecaa92ea795db43a366e4c91a9d0f26159331f
pebrieriawan <pebrieriawan13@gmail.com>
2024-12-02 16:28:48 +0800

    Update profile page layout

--- app/src/main/java/com/dicoding/kaliatra/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/dicoding/kaliatra/ui/scan/ScanFragment.kt
@@ -1,6 +1,5 @@
 package com.dicoding.kaliatra.ui.scan
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -10,7 +9,6 @@ import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
 import com.dicoding.kaliatra.R
 import com.dicoding.kaliatra.databinding.FragmentScanBinding
-import com.dicoding.kaliatra.ui.result.ResultActivity
 
 class ScanFragment : Fragment() {
 
@@ -25,44 +23,37 @@ class ScanFragment : Fragment() {
         val scanViewModel = ViewModelProvider(this)[ScanViewModel::class.java]
 
         _binding = FragmentScanBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val galleryButton = binding.galleryButton
-        val cameraButton = binding.cameraButton
-        val analyzeButton = binding.analyzeButton
-
-        galleryButton.setOnClickListener {
-            // Tambahkan aksi untuk tombol Galeri
-        }
-
-        cameraButton.setOnClickListener {
-            // Tambahkan aksi untuk tombol Kamera
-        }
-
-        analyzeButton.setOnClickListener {
-            // Tambahkan aksi untuk tombol Analisis
-        }
-
-        val menuHost: MenuHost = requireActivity()
-        menuHost.addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.menu_main, menu)
+        return binding.apply {
+            galleryButton.setOnClickListener {
+                // Tambahkan aksi untuk tombol Galeri
             }
 
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return when (menuItem.itemId) {
-                    R.id.action_history -> {
-                        val intent = Intent(requireContext(), ResultActivity::class.java)
-                        startActivity(intent)
-                        true
-                    }
+            cameraButton.setOnClickListener {
+                // Tambahkan aksi untuk tombol Kamera
+            }
 
-                    else -> false
+            analyzeButton.setOnClickListener {
+                // Tambahkan aksi untuk tombol Analisis
+            }
+
+            val menuHost: MenuHost = requireActivity()
+            menuHost.addMenuProvider(object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.menu_main, menu)
                 }
-            }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
-        return root
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    return when (menuItem.itemId) {
+                        R.id.action_history -> {
+                            // Tambahkan aksi untuk menu Riwayat
+                            true
+                        }
+
+                        else -> false
+                    }
+                }
+            }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        }.root
     }
 
     override fun onDestroyView() {
