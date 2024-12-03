070361945e6d5958a3f7483dd67632cc3af5f268
pebrieriawan <pebrieriawan13@gmail.com>
2024-12-03 18:00:37 +0800

    Add history page

---
+++ app/src/main/java/com/dicoding/kaliatra/ui/history/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.kaliatra.ui.history
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.kaliatra.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
